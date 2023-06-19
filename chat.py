e6f83bda071c86edb0027f6b21965d16cf59a94c
peyton005 <peytond005@gmail.com>
2023-06-19 14:28:20 -0500

    first commit

---
+++ chat.py
@@ -0,0 +1,15 @@
+import os
+import openai
+
+openai.api_key = "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"
+
+response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.",
+    tempurature=0.9,
+    max_tokens=150,
+    top_p=1,
+    frequency_penalty=0.0,
+    presence_penalty=0.6,
+    stop=[" Human", " AI:"]
+)
