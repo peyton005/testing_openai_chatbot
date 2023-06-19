274b6714c1a19689a8badd9f236e7509e9c577cd
peyton005 <peytond005@gmail.com>
2023-06-19 17:39:16 -0500

    added text to chat.py for debugging

--- chat.py
+++ chat.py
@@ -3,6 +3,7 @@ import openai
 
 openai.api_key = "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"
 
+print("This is an auto-generated text sequence by openai using their LLM API: ")
 response = openai.Completion.create(
     model="text-davinci-003",
     prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.",
